ce6f70c7b9b5ce81ceb3d53d3915002ce9c5360a
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-05-12 18:50:02 +0700

    menambahkan class ibu beserta constructor dan destructor

---
+++ ibu.h
@@ -0,0 +1,25 @@
+#ifndef IBU_H
+#define IBU_H
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+#include <string>
+#include "anak.h"
+using namespace std;
+
+#include <vector>
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+    ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+     ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+};
+#endif
